f53266486444e3b062f7d4c8f1e62b579aa66a1c
GHOST-Aram <owinoojala@gmail.com>
2022-12-20 15:40:07 +0300

    validate phone and password2

--- script.js
+++ script.js
@@ -3,30 +3,13 @@ const password1 = document.querySelector('#password-1')
 const password2 = document.querySelector('#password-2')
 const email = document.querySelector('#email')
 const form = document.querySelector('form')
+const phone = document.querySelector('input[type=tel]')
 
 
-function validatePassword(password1, password2){
-    // Strong password regex
-    const regex =     /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{12,})/
-    const errorMsg = document.querySelector('#psw-error-msg')
-    if(password1.value !== password2.value){
-        errorMsg.textContent = 'Passwords do not match'
-        errorMsg.style.color = 'var(--danger-color)'
-        password1.style.borderColor = 'var(--danger-color)'       
-        password2.style.borderColor = 'var(--danger-color)'       
+
+function trimPhone(){
+    phone.value = phone.value.replace(' ','')
     }
-    else
-        if(regex.test(password1.value)){
-            errorMsg.textContent = 'Strong Password'
-            password1.style.borderColor = 'var(--success-color)'       
-            password2.style.borderColor = 'var(--success-color)' 
-            errorMsg.style.color = 'var(--success-color)'
-        }
-        else{
-            errorMsg.textContent = 'Use Stronger Password'
-
-        }
-}
 
 function validatePassword1(e){
     const regex =     /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{12,})/
@@ -38,28 +21,30 @@ function validatePassword1(e){
         
     }
     else{
-        errorMsg.textContent = 'Use Stronger Password'
+        errorMsg.textContent = 'Use Stronger alphanumeric Password'
         errorMsg.style.color = 'var(--danger-color)'
     }
 }
 
 function validatePassword2(){
     const errorMsg = document.querySelector('#psw-error-msg')
-    if(password1.value === password2.value)
+    if(password1.value === password2.value){
         password2.style.borderColor = 'var(--success-color)' 
+        errorMsg.textContent = ''
+    }
     else {
         errorMsg.textContent = 'Passwords do not match'
-        errorMsg.style.color = 'var(--danger-color)'
-        password1.style.borderColor = 'var(--danger-color)'       
+        errorMsg.style.color = 'var(--danger-color)'      
         password2.style.borderColor = 'var(--danger-color)'       
     }
 
 }
+phone.addEventListener('input', trimPhone)
 password1.addEventListener('input', validatePassword1)
-password2.addEventListener('blur', validatePassword2)
+password2.addEventListener('input', validatePassword2)
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
-    validatePassword(password1, password2)
+    
 
 })
